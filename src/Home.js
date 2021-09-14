508af54662052b2dddbaa6f33b5d4b62a9278a3f
muthu <mkandanmk7@gmail.com>
2021-09-15 00:16:26 +0530

    LInk

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,13 @@
-function Home() {
-  return <p>Home</p>;
+function Home({ history }) {
+  return (
+    <>
+      <p>Home</p>
+      <button onClick={history.goBack}>Go forward</button>
+      <button onClick={history.goForward}>Go backward</button>
+      <button onClick={history.push("/post")}>Go push</button>
+      <button onClick={history.replace("/post")}>Go replace</button>
+    </>
+  );
 }
 
 export default Home;
--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,5 @@
-function Post() {
+function Post(props) {
+  console.log(props);
   return <p>Posts</p>;
 }
 export default Post;
